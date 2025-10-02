b72b230c5a3e55104c8973025a104fcb41d947cf
GAGANDEEP <gagandeep982006@gmail.com>
2025-10-02 22:31:35 +0530

    Update GREATEST-FROM-3.cpp

--- SHORT-PROGRAMS/GREATEST-FROM-3.cpp
+++ SHORT-PROGRAMS/GREATEST-FROM-3.cpp
@@ -6,12 +6,4 @@ int main(){
     (arr[0]<arr[2] ) ? printf("%d",arr[2]) : printf("%d",arr[0]);
 
     return 0;
-}#include<stdio.h>
-
-int main(){
-    int arr[3] = {1,5,2};
-    (arr[0]<arr[1])? (arr[0] = arr[1]): (arr[0] = arr[0]);
-    (arr[0]<arr[2] ) ? printf("%d",arr[2]) : printf("%d",arr[0]);
-
-    return 0;
 }
